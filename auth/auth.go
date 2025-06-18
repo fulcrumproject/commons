f594cd2b0fc80e2f8a52ab5bfed7d13b9ec7af43
Ernesto Colombo <ernesto.colombo@contactlab.com>
2025-06-18 14:23:28 +0200

    DefaultObjectScope

--- auth/auth.go
+++ auth/auth.go
@@ -45,6 +45,52 @@ func (a AllwaysMatchObjectScope) Matches(identity *Identity) bool {
 	return true // Always matches, used for global actions
 }
 
+// DefaultObjectScope is the default implementation of ObjectScope
+type DefaultObjectScope struct {
+	ParticipantID *properties.UUID
+	ProviderID    *properties.UUID
+	ConsumerID    *properties.UUID
+	AgentID       *properties.UUID
+}
+
+// Matches checks if the given identity matches the object scope
+func (target *DefaultObjectScope) Matches(id *Identity) bool {
+	if id == nil {
+		return false
+	}
+
+	// If all fields are nil in the caller scope, it has unrestricted access (admin)
+	if id.Scope.ParticipantID == nil && id.Scope.AgentID == nil {
+		return true
+	}
+
+	// If all fields are nil in the target scope, global access is allowed
+	if target.ParticipantID == nil && target.ProviderID == nil && target.ConsumerID == nil && target.AgentID == nil {
+		return true
+	}
+
+	// Participant check: If identity requires a participant, caller must have same participant or provider or consumer
+	if id.Scope.ParticipantID != nil {
+		if target.ParticipantID != nil && *target.ParticipantID == *id.Scope.ParticipantID {
+			return true
+		}
+		if target.ProviderID != nil && *target.ProviderID == *id.Scope.ParticipantID {
+			return true
+		}
+		if target.ConsumerID != nil && *target.ConsumerID == *id.Scope.ParticipantID {
+			return true
+		}
+	}
+
+	// Agent check: If source requires an agent, caller must have same agent
+	if id.Scope.AgentID != nil && target.AgentID != nil && *target.AgentID == *id.Scope.AgentID {
+		return true
+	}
+
+	return false
+
+}
+
 // Identity implements the Identifier interface
 type Identity struct {
 	ID    properties.UUID
--- go.mod
+++ go.mod
@@ -12,7 +12,7 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/ajg/form v1.5.1 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
 	github.com/go-jose/go-jose/v4 v4.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -21,7 +21,7 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	golang.org/x/crypto v0.39.0 // indirect
-	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/net v0.41.0 // indirect
 	golang.org/x/oauth2 v0.30.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
